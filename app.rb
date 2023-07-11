7285f5aae6c239b8feb670d59d6333f2926cb417
jbenzer <136658052+jbenzer@users.noreply.github.com>
2023-07-11 15:50:07 +0000

    commits

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ get('/') do
     <a href=\"Play_Scissors\">Play Scissors</a><br>
   </p>
   <h1>Welcome to Rock-Paper-Scissors!</h1>
-  <p>From <a href=\"Wiki_link\">Wikipedia</a>:</p>
+  <p>From <a href=\"https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors\" target=_blank>Wikipedia</a>:</p>
   <p>Rock-paper-scissors (also known as paper, scissors, stone or other variants) is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand.</p>
   <p>These shapes are:</p>
   <ul>
@@ -74,5 +74,98 @@ get('/') do
   </div>
   <p>
   Mushi-ken, the earliest Japanese sansukumi-ken game (1809). From left to right: slug (namekuji), frog (kawazu) and snake (hebi).</p>
+  "
+end
+
+get("/Play_Rock") do
+  
+  message0 = "We played rock!"
+  options = ["rock", "paper", "scissors"]
+  pick = options.sample()
+  if pick == "rock"
+    message1 = "They played rock!"
+    message2 = "We tied!"
+  elsif pick == "paper"
+    message1 = "They played paper!"
+    message2 = "We lost!"
+  else
+    message1 = "They played scissors!"
+    message2 = "We won!"
+  end
+
+  "
+  <p>
+    <a href=\"Play_Rock\">Play Rock</a><br>
+    <a href=\"Play_Paper\">Play Paper</a><br>
+    <a href=\"Play_Scissors\">Play Scissors</a><br>
+  </p>
+  <h2>#{message0}</h2>
+  <h2>#{message1}</h2>
+  <h2>#{message2}</h2>
+  <p>
+    <a href=\"/\">Rules</a>
+  </p>
+  "
+end
+
+get("/Play_Paper") do
+  
+  message0 = "We played paper!"
+  options = ["rock", "paper", "scissors"]
+  pick = options.sample()
+  if pick == "rock"
+    message1 = "They played rock!"
+    message2 = "We won!"
+  elsif pick == "paper"
+    message1 = "They played paper!"
+    message2 = "We tied!"
+  else
+    message1 = "They played scissors!"
+    message2 = "We lost!"
+  end
+
+  "
+  <p>
+    <a href=\"Play_Rock\">Play Rock</a><br>
+    <a href=\"Play_Paper\">Play Paper</a><br>
+    <a href=\"Play_Scissors\">Play Scissors</a><br>
+  </p>
+  <h2>#{message0}</h2>
+  <h2>#{message1}</h2>
+  <h2>#{message2}</h2>
+  <p>
+    <a href=\"/\">Rules</a>
+  </p>
+  "
+end
+
+get("/Play_Scissors") do
+  
+  message0 = "We played scissors!"
+  options = ["rock", "paper", "scissors"]
+  pick = options.sample()
+  if pick == "rock"
+    message1 = "They played rock!"
+    message2 = "We lost!"
+  elsif pick == "paper"
+    message1 = "They played paper!"
+    message2 = "We won!"
+  else
+    message1 = "They played scissors!"
+    message2 = "We tied!"
+  end
+
+  "
+  <p>
+    <a href=\"Play_Rock\">Play Rock</a><br>
+    <a href=\"Play_Paper\">Play Paper</a><br>
+    <a href=\"Play_Scissors\">Play Scissors</a><br>
+  </p>
+  <h2>#{message0}</h2>
+  <h2>#{message1}</h2>
+  <h2>#{message2}</h2>
+  <p>
+    <a href=\"/\">Rules</a>
+  </p>
   "
 end
